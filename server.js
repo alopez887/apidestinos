02456536b7a67623f8583a733915a274ddd10ba1
alberto <nkmsistemas@gmail.com>
2025-09-22 12:13:44 -0700

    Generando API para lista de salida Destino

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import loginUsuario from './loginUsuario.js';
 import { obtenerReservaTours } from './obtenerReservaTours.js';
 import actualizarDatosTours from './actualizarDatosTours.js';
 import guardarFirmaTours from './firmas/guardarFirmaTours.js'; // <- este es el handler correcto
+import consultarSalidasTours from './consultarSalidasTours.js';
 
 // ⬇️ NUEVO: importar el handler de hoteles
 import consultarHoteles from './consultarHoteles.js';
@@ -100,6 +101,8 @@ app.post('/api/actualizar-datos-tours', actualizarDatosTours);
 
 // ⬇️ NUEVO: endpoint de hoteles para Destinos/Tours (solo nombres)
 app.get('/hoteles', consultarHoteles);
+
+app.get('/api/salidas-tours', consultarSalidasTours);
 
 // Healthcheck
 app.get('/', (_req, res) => res.send('API Destinos activa 🎯'));
